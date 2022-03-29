7064adcff7cf685107a2005a4b605b87aa032301
Daineanu Denis <denis.daineanu@gmail.com>
2022-03-29 14:52:58 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/SoftbinatorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftbinatorProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftbinatorProjectApplication.class, args);
+    }
+
+}
